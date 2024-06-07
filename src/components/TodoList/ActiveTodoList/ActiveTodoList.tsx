40094d194bdea9d24e8a561239e6c13820c8dbae
maxim polosuhin <maksim.polosuhin@nordclan.com>
2024-06-07 22:16:59 +0500

    fix

--- src/components/TodoList/ActiveTodoList/ActiveTodoList.tsx
+++ src/components/TodoList/ActiveTodoList/ActiveTodoList.tsx
@@ -1,35 +1,34 @@
-import {Card, Col, Divider, List, Row, Typography} from "antd";
+import { Card, Col, Divider, List, Row, Typography } from "antd";
 import Title from "antd/lib/typography/Title";
-import {FC} from "react";
-import {TodoType} from "../../../types/types.ts";
-import {getActiveTodos} from "../../../store/selectors/selectors.ts";
-import {useAppSelector} from "../../../store/store.ts";
+import { FC } from "react";
+import { TodoType } from "../../../types/types.ts";
+import { getActiveTodos } from "../../../store/selectors/selectors.ts";
+import { useAppSelector } from "../../../store/store.ts";
 
 const ActiveTodoList: FC = () => {
-    const { Text } = Typography;
-    const activeTodos:TodoType[] = useAppSelector(getActiveTodos)
-    console.log(activeTodos)
+  const { Text } = Typography;
+  const activeTodos: TodoType[] = useAppSelector(getActiveTodos);
 
-    return (
-        <Col className="todo-list">
-            <Card>
-                <Title level={4}>Active Todo list</Title>
-                <Divider type="horizontal" />
-                {activeTodos.length === 0 ? (
-                    <Row>
-                        <Text>Everything is done, you are great!</Text>
-                    </Row>
-                ) : (
-                    <List
-                        size="large"
-                        bordered
-                        dataSource={activeTodos}
-                        renderItem={(item) => <List.Item>{item.name}</List.Item>}
-                    />
-                )}
-            </Card>
-        </Col>
-    );
-}
+  return (
+    <Col className="todo-list">
+      <Card>
+        <Title level={4}>Active Todo list</Title>
+        <Divider type="horizontal" />
+        {activeTodos.length === 0 ? (
+          <Row>
+            <Text>Everything is done, you are great!</Text>
+          </Row>
+        ) : (
+          <List
+            size="large"
+            bordered
+            dataSource={activeTodos}
+            renderItem={(item) => <List.Item>{item.name}</List.Item>}
+          />
+        )}
+      </Card>
+    </Col>
+  );
+};
 
 export default ActiveTodoList;
